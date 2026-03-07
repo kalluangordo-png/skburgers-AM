c633ba1cb71cdfb5dff7a765db1cf98c2e1ff061
Kalluan <kalluangordo@gmail.com>
2026-03-07 18:35:06 -0300

    Removendo .env e fixando config do Firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -6,21 +6,21 @@ import {
   persistentMultipleTabManager 
 } from 'firebase/firestore';
 
-// Colocando os dados direto aqui para a tela branca sumir
+// Agora os dados estão fixos, não precisa mais do arquivo .env para o Firebase
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBh3hP90N3atIJO1o1gd0PfBdDYeM2YVVA",
+  authDomain: "skburgers.firebaseapp.com",
+  projectId: "skburgers",
+  storageBucket: "skburgers.firebasestorage.app",
+  messagingSenderId: "272321954556",
+  appId: "1:272321954556:web:41bb8cd7c357e39209cd79",
+  measurementId: "G-BEW46FDE2Z"
 };
 
 const app = initializeApp(firebaseConfig);
 export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 
-// Resiliência SK-Maps: Ativar experimentalForceLongPolling e persistentLocalCache
+// Resiliência SK-Maps
 export const db = initializeFirestore(app, {
   localCache: persistentLocalCache({
     tabManager: persistentMultipleTabManager()
